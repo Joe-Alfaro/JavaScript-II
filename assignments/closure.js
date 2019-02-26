1317d19ab3fef6820a952ae884b55a64a13c027e
joe-alfaro <josephlalfaro@gmail.com>
2019-02-26 14:43:07 -0800

    finished closure challenge 3

--- assignments/closure.js
+++ assignments/closure.js
@@ -24,29 +24,26 @@ const counter = () => {
 // console.log(newCounter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = (direction) => {
+const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-  let counter = 0;
-  if (direction === 'increment'){
-    return () => ++counter;
-  }
-  if (direction === 'decrement'){
-    return () => --counter;
-  }
+  let counter = 0; 
+  return (direction) => direction === 'increment' ? ++counter : direction ==='decrement' ? --counter : console.log('None') ;
+    // if (direction === 'increment') {
+    //   return ++counter;
+    // }
+    // if (direction ==='decrement'){
+    //   return --counter;
+    // }
 };
 
 //Example usage: 
-let increment = counterFactory('increment');
-let decrement = counterFactory('decrement');
-console.log(increment()); // 1
-console.log(decrement()); // 0
-console.log(decrement()); // 0
-console.log(decrement()); // 0
-console.log(increment());
-console.log(increment()); // 1
-console.log(decrement()); // 0
-console.log(decrement()); // 0
-console.log(decrement()); // 0
-console.log(increment());
+// const newCounter = counterFactory();
+
+// console.log(newCounter('increment'));
+// console.log(newCounter('increment'));
+// console.log(newCounter('increment'));
+// console.log(newCounter('decrement'));
+// console.log(newCounter('decrement'));
+// console.log(newCounter('decrement'));
